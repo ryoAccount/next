f5c67ffcb71902217d73db3c31944bf62ad75351
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-08-10 23:51:09 +0900

    json-server

--- src/app/component/TodoList.tsx
+++ src/app/component/TodoList.tsx
@@ -1,15 +1,22 @@
+import { Todo } from '@/types'
 import React from 'react'
 
-const TodoList = () => {
+interface TodoListProps {
+  todos: Todo[]
+}
+
+const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className='space-y-3'>
-      <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
-        <span>walking</span>
-        <div>
-          <button className='text-green-500 mr-3'>edit</button>
-          <button className='text-red-500'>delete</button>
-        </div>
-      </li>
+      {todos.map(todo => (
+        <li key={todo.id} className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
+          <span> {todo.text} </span>
+          <div>
+            <button className='text-green-500 mr-3'>edit</button>
+            <button className='text-red-500'>delete</button>
+          </div>
+        </li>
+      ))}
     </ul>
   )
 }
