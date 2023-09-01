bce6c92beecdab8763af4d4fae2fe81ab930652c
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-09-02 02:03:22 +0900

    blog metadata

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -41,13 +41,24 @@ export default function Home({ contents, totalCount }) {
           <p>This is a blog where I tweet when busy, when having a hard time, when having fun, when moved, or when I want to express my feelings in words.</p>
           <p>The site name is Monologue because it means something like a whisper, not something you send to someone.</p>
         </div>
+        <hr />
         <h5>About link</h5>
         <div>
           <dl className="inline">
             <dt>site name</dt>
-              <dd>Monologue</dd>
+            <dd>Monologue</dd>
             <dt>url</dt>
-              <dd><a href="https://blog-ryocv.vercel.app/">https://blog-ryocv.vercel.app/</a></dd>
+            <dd><a href="https://blog-ryocv.vercel.app/">https://blog-ryocv.vercel.app/</a></dd>
+          </dl>
+        </div>
+        <hr />
+        <h5>About me</h5>
+        <div>
+          <dl className="inline">
+            <dt>site name</dt>
+            <dd>RYO CV</dd>
+            <dt>portfolio</dt>
+            <dd><a href="https://ryoaccount.github.io/profile" target="_blank">https://ryoaccount.github.io/profile</a></dd>
           </dl>
         </div>
 
