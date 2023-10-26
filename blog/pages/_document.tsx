6694bd7f904105d74bbbf398ea0d479e106e87a6
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-10-27 02:58:46 +0900

    bg image

--- blog/pages/_document.tsx
+++ blog/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;500&display=swap" rel="stylesheet" />
       </Head>
       <body>
+        <span id='bg-image'></span>
         <HeaderData />
         <Main />
         <FooterData />
