50c7aeb71f5e0d4d31313d87e731e5455e12d846
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-11-01 00:52:27 +0900

    Additional features

--- portforio/app/blog/[slug]/page.js
+++ portforio/app/blog/[slug]/page.js
@@ -1,52 +1,26 @@
 import Image from "next/image";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
+import PrevNext from "@/app/components/prevNext";
+import { getAllBlogs, getSingleBlog } from "@/app/utils/mdQueries";
 
 export async function generateStaticParams() {
-  async function getAllBlogs() {
-    const files = fs.readdirSync(path.join("data"));
-
-    const blogs = files.map((filename) => {
-      const slug = filename.replace(".md", "");
-      const fileData = fs.readFileSync(path.join("data", filename), "utf-8");
-      const { data } = matter(fileData);
-
-      return {
-        frontmatter: data,
-        slug: slug,
-      };
-    });
-
-    return {
-      blogs: blogs,
-    };
-  }
-
   const { blogs } = await getAllBlogs();
   const paths = blogs.map((blog) => `/${blog.slug}`);
   return paths;
 }
 
-async function getSingleBlog(context) {
-  const { slug } = context.params;
-  const data = await import(`../../../data/${slug}.md`);
-  const singleDocument = matter(data.default);
-
-  return {
-    singleDocument: singleDocument,
-  };
-}
-
 const SingleBlog = async (props) => {
   const { singleDocument } = await getSingleBlog(props);
+  const { blogs } = await getAllBlogs();
+  const prev = blogs.filter((blog) => blog.frontmatter.id === singleDocument.data.id - 1);
+  const next = blogs.filter((blog) => blog.frontmatter.id === singleDocument.data.id + 1);
   return (
     <div>
       <Image src={singleDocument.data.image} alt="blog-image" height={500} width={1000} quality={90} priority={true} />
       <h1>{singleDocument.data.title}</h1>
       <p>{singleDocument.data.date}</p>
       <ReactMarkdown>{singleDocument.content}</ReactMarkdown>
+      <PrevNext prev={prev} next={next} />
     </div>
   );
 };
--- portforio/app/blog/page.js
+++ portforio/app/blog/page.js
@@ -1,31 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-
-async function getAllBlogs() {
-  const files = fs.readdirSync(path.join("data"));
-
-  const blogs = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-    const fileData = fs.readFileSync(path.join("data", filename), "utf-8");
-    const { data } = matter(fileData);
-
-    return {
-      frontmatter: data,
-      slug: slug,
-    };
-  });
-
-  const orderedBlogs = blogs.sort((a, b) => {
-    return b.frontmatter.id - a.frontmatter.id;
-  });
-
-  return {
-    blogs: orderedBlogs,
-  };
-}
+import { getAllBlogs } from "../utils/mdQueries";
 
 const Blog = async () => {
   const { blogs } = await getAllBlogs();
